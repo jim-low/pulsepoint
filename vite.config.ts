2975eeb19d08d57195eed6efe68403115b3ef18a
jim-low <robil12380@gmail.com>
2024-10-27 19:36:47 +0800

    fix(build): fix base URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pulsepoint',
+  base: '/jim-low.github.io/pulsepoint',
   plugins: [react()],
 })
